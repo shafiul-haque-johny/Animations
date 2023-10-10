08b23110ee2916dc98237a5e52d51ff06fad4ebc
shafiul-haque-johny <mhaque171319@bscse.uiu.ac.bd>
2023-10-10 19:19:52 +0600

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:animation_starter_code/implicit_examples/animated_color_pallete.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Animations',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const AnimatedColorPalette(),
+    );
+  }
+}

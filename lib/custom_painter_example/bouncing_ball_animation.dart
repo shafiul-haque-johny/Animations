b8ddd3604c8ca90240b6e1928646a47cb750f5ce
shafiul-haque-johny <mhaque171319@bscse.uiu.ac.bd>
2023-10-23 16:33:04 +0600

    bouncing ball animation

--- lib/custom_painter_example/bouncing_ball_animation.dart
+++ lib/custom_painter_example/bouncing_ball_animation.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 class BouncingBallAnimation extends StatefulWidget {
@@ -7,7 +8,49 @@ class BouncingBallAnimation extends StatefulWidget {
   State<BouncingBallAnimation> createState() => _BouncingBallAnimationState();
 }
 
-class _BouncingBallAnimationState extends State<BouncingBallAnimation> {
+class _BouncingBallAnimationState extends State<BouncingBallAnimation>
+    with SingleTickerProviderStateMixin {
+  
+  late AnimationController controller;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 1500,
+      ),
+    );
+
+    controller.addListener(() {
+      if (animation.isCompleted)
+      {
+        controller.reverse();
+      }
+      else if (animation.isDismissed){
+        controller.forward();
+      }
+    });
+
+    animation = Tween<double>(begin: 0, end: 1).animate(controller);
+
+    // OR use
+    /*animation.addStatusListener((status) { 
+      if (status == AnimationStatus.completed)
+      {
+        controller.reverse();
+      }
+      else if (status == AnimationStatus.dismissed){
+        controller.forward();
+      }
+    }); */
+
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,10 +58,37 @@ class _BouncingBallAnimationState extends State<BouncingBallAnimation> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            // Need Ball
+            AnimatedBuilder(
+              animation: animation,
+              builder: (context, child) => CustomPaint(
+                size: const Size(250, 250),
+                painter: BouncingBallpainter(animation.value),
+              ),
+            ),
           ],
         ),
       ),
     );
+  }
+}
+
+class BouncingBallpainter extends CustomPainter {
+  
+  final double animationValue;
+
+  BouncingBallpainter(this.animationValue);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawCircle(
+      Offset(size.width, size.height - (size.height * animationValue),),
+      20,
+      Paint()..color = Colors.green,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
